e832a7724958021ac25bc9c6dbead5a40e35f2bb
isEstaraCodeglgit <2785588657@qq.com>
2026-03-25 20:20:09 +0800

    初次上传Vue3路由演示

---
+++ src/components/List.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="page">
+		<h1>列表页</h1>
+		<p>这里有一些数据...</p>
+		   <router-link to="/">
+		      <button>返回首页</button>
+		    </router-link>
+		<router-link to="/detail/101">
+			<button>去详情页</button>
+		</router-link>
+	</div>
+</template>

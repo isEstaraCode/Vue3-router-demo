2baa739dbdfc6d95680a4b6d31da04a0e11f0823
isEstaraCode <2785588657@qq.com>
2026-03-26 14:44:33 +0800

    Add files via upload

---
+++ src/components/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="page">
+		<h1>详情页</h1>
+		<p>当前查看ID：{{ route.params.id }}</p>
+		<div class="actions">
+			<button @click="goBack">返回上一页</button>
+			<button @click="goForward">前进下一页</button>
+			<button @click="goHome">强制返回首页</button>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	import { useRouter,useRoute } from 'vue-router';
+	const router=useRouter();
+	const route=useRoute();
+	const goBack=()=>{router.back();};
+	const goForward=()=>{router.forward();};
+	const goHome=()=>{router.push('/');};
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="page">
+		<h1>首页</h1>
+		<p>这是首页..</p>
+		<router-link to="/list">
+			<button>去列表页</button>
+		</router-link>
+		<router-link to="/detail/101">
+			<button>去详情页</button>
+		</router-link>
+	</div>
+</template>
